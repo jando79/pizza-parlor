357567027d0b93b1afc0a63f1edd3298cca65986
David Jandron <jandron1211@gmail.com>
2023-01-27 10:20:50 -0800

    ADD TDD content, and WIP js code

---
+++ TDD.md
@@ -0,0 +1,6 @@
+Describe: PizzaParlor()
+
+Test: "It should collect pizza choices"
+Code: 
+  
+Expected Output: Pizza { toppings: ["anchovies", "pineapple"], size: "medium" }
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
-Describe: PizzaParlor()
+//Business Logic
 
-Test: "It should return a Pizza object with two properties for toppings and size"
-Code: const myPizza = new Pizza(["anchovies", "pineapple"], "medium");
-Expected Output: Pizza { toppings: ["anchovies", "pineapple"], size: "medium" }
+function PizzaParlor() {
+  this.myPizza = {};
+  this.currentId = 0;
+};
+
+PizzaParlor.prototype.addPizza = function(pizza) {
+  pizza.id = this.assignId();
+  this.myPizza[pizza.id] = pizza;
+};
+
+PizzaParlor.prototype.assignId = function() {
+  this.current += 1;
+  return this.currentId;
+};
+
